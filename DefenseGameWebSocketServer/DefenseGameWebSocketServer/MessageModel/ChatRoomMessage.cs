16172e164eaf060a0533832839b35d33b471bbfc
SeminL22 <98643055+SeminL22@users.noreply.github.com>
2025-07-29 16:56:59 +0900

    채팅 구현

---
+++ DefenseGameWebSocketServer/DefenseGameWebSocketServer/MessageModel/ChatRoomMessage.cs
@@ -0,0 +1,17 @@
+﻿namespace DefenseGameWebSocketServer.Model
+{
+    public class ChatRoomMessage : BaseMessage
+    {
+        public string playerId { get; set; }
+        public string message { get; set; }
+        public ChatRoomMessage(
+            string playerId,
+            string message
+        )
+        {
+            type = "chat_room";
+            this.playerId = playerId;
+            this.message = message;
+        }
+    }
+}
